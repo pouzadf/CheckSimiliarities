eb8b69cc189fcf0d4b20627d425f19d9f73cc2de
pouzadf <florian.pouzada@epita.fr>
2020-12-24 14:42:22 +0100

    [TESTS] adds tests for tokenize ia function

--- tests/ia_test.py
+++ tests/ia_test.py
@@ -1,5 +1,22 @@
 from IA.check_plagiarism import *
+from utils import *
 
 
-def test_answer():
-    assert 5 == 5
+def testSequenceListSize_whenTokenized_withOddStrideSize():
+    sentence = create_random_sentence()
+    sequences = window_tokenizer(sentence, window_size = 2, strides = 2)
+    assert len(sequences) == 5
+
+
+def testSequenceListSize_whenTokenized_withNotOddStrideSize():
+    sentence = create_random_sentence()
+    sequences = window_tokenizer(sentence, window_size = 2, strides = 3)   
+    [print(seq.text) for seq in sequences]
+    assert len(sequences) == 3
+
+
+def testSequenceSize_whenTokenized_withOddWindowSize():
+    sentence = create_random_sentence()    
+    sequences = window_tokenizer(sentence, window_size = 5, strides = 5)    
+    for seq in sequences:        
+        assert len(seq.text.split()) == 5
