638ea3ee91b59117d1f5c86c454ca4e66534f3b3
pouzadf <florian.pouzada@epita.fr>
2020-12-26 01:37:55 +0100

    [TESTS] adds tests over help message and unknown args

--- tests/ia_test.py
+++ tests/ia_test.py
@@ -1,22 +1,36 @@
 from IA.check_plagiarism import *
+from main import *
 from utils import *
+from pytest import *
+
+def testDisplayHelpMessage_WritesOnStdout(capsys):
+    display_help_message()
+    captured = capsys.readouterr() 
+    assert len(captured.out) > 0
+
+def testHandleUnknownArgProvided_WritesOnStdoutAndStdErr(capsys):
+    handle_unknown_arg_provided()
+    captured = capsys.readouterr()
+    assert len(captured.out) > 0
+    assert len(captured.err) > 0
 
 
-def testSequenceListSize_whenTokenized_withOddStrideSize():
-    sentence = create_random_sentence()
-    sequences = window_tokenizer(sentence, window_size = 2, strides = 2)
-    assert len(sequences) == 5
+def testHandleUnknownArgProvided_WritesRightMessageOnStdErr(capsys):
+    handle_unknown_arg_provided()
+    captured = capsys.readouterr()
+    assert captured.err == "Unknown args have been provided\n"   
 
 
-def testSequenceListSize_whenTokenized_withNotOddStrideSize():
-    sentence = create_random_sentence()
-    sequences = window_tokenizer(sentence, window_size = 2, strides = 3)   
-    [print(seq.text) for seq in sequences]
-    assert len(sequences) == 3
-
-
-def testSequenceSize_whenTokenized_withOddWindowSize():
-    sentence = create_random_sentence()    
-    sequences = window_tokenizer(sentence, window_size = 5, strides = 5)    
-    for seq in sequences:        
-        assert len(seq.text.split()) == 5
+def testDisplayHelpMessage_WritesCorrectMsgOnStdout(capsys):
+    display_help_message()
+    captured = capsys.readouterr() 
+    help_sentences = [seq for seq in captured.out.splitlines() if len(seq) > 0]
+    assert "usage: CheckSimilarities [-t] [-h] fname fname2" == help_sentences[0]
+    assert "compute and display sentence similarities between text files" == help_sentences[1]
+    assert "positional arguments:" == help_sentences[2]
+    assert "fname        path to the first file to compare" == help_sentences[3]
+    assert "fname2       path to the second file to compare" == help_sentences[4]
+    assert "optional arguments:" == help_sentences[5]
+    assert "-h, --help   Show this help message " == help_sentences[6]
+    assert "-t, --tests  Run the testsuite over the progam and exit" == help_sentences[7]
+    
